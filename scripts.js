016f195c5d94503c56117dc5d44e2ffab7c4f7e9
Cathy Du <cathyy.du@gmail.com>
2018-01-05 01:34:54 -0500

    First commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(function() {
+	$("#dropdown-menu").click(function(){
+	    $("#menu-options").fadeToggle(100);
+	});
+});
+
+
+
+$(document).ready(function() {
+
+$('body').css('display', 'none');
+
+$('body').fadeIn(1000);
+
+
+
+$('.link').click(function(event) {
+
+event.preventDefault();
+
+newLocation = this.href;
+
+$('body').fadeOut(1000, newpage);
+
+});
+
+
+
+function newpage() {
+
+window.location = newLocation;
+
+}
+
+});
